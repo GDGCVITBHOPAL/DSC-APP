6823e8c316a123bf5efcb03555c90d95f3349e80
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-29 23:36:08 +0530

    app bar: Add app drawer

--- lib/widgets/navigation.dart
+++ lib/widgets/navigation.dart
@@ -1,7 +1,9 @@
 import 'package:dsc_client/configs/assets.dart';
 import 'package:dsc_client/screens/MediumPage/ArticlesPage.dart';
+import 'package:dsc_client/screens/dscScreen/aboutDSC.dart';
 import 'package:dsc_client/screens/homeScreen/home.dart';
 import 'package:dsc_client/screens/requestEvents/requestEvent.dart';
+import 'package:dsc_client/screens/team/team.dart';
 import 'package:flutter/material.dart';
 
 class navigate extends StatefulWidget {
@@ -18,20 +20,55 @@ class _navigateState extends State<navigate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer:
+          Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                 DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Image.asset(Assets.vitb_dsc_logo),
+                ),
+                ListTile(
+                  title: const Text('About Us'),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => AboutDSC()));
+                    //Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Our Team'),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => Team()));
+                  },
+                ),
+              ],
+            ),
+          ),
+
       appBar: AppBar(
         toolbarHeight: 80,
         elevation: 0,
         title:
       Container(
           height: 80,
-          child: Center(child: Image.asset(Assets.vitb_dsc_logo)),
+          child: Image.asset(Assets.vitb_dsc_logo),
         ),
+        centerTitle: true,
         backgroundColor: Colors.white,
       ),
       body: screens[index],
-      bottomNavigationBar: NavigationBarTheme(data:
+      bottomNavigationBar: NavigationBarTheme(
+        data:
       NavigationBarThemeData(
-        indicatorColor: Colors.green.shade400,
+        indicatorColor: Colors.blue.shade100,
+        // iconTheme: MaterialStateProperty(),
         height: 60,
         labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
       ),
