ed8a958bcbfaa5f88372f1f5a5acac1a74980132
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-30 19:13:36 +0530

    navigation bar: Implement  indexed stack

--- lib/widgets/navigation.dart
+++ lib/widgets/navigation.dart
@@ -14,71 +14,83 @@ class navigate extends StatefulWidget {
 }
 
 class _navigateState extends State<navigate> {
-  int index =0;
-  final screens=[Home(), ArticlesPage(),RequestEventForm()];
+  int current_index = 0;
+  final screens = [Home(), ArticlesPage(), RequestEventForm()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer:
-          Drawer(
-            // Add a ListView to the drawer. This ensures the user can scroll
-            // through the options in the drawer if there isn't enough vertical
-            // space to fit everything.
-            child: ListView(
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.zero,
-              children: [
-                 DrawerHeader(
-                  decoration: BoxDecoration(
-                    // color: Colors.white,
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  // color: Colors.white,
                   ),
-                  child: Image.asset(Assets.vitb_dsc_logo),
-                ),
-                ListTile(
-                  title: const Text('About Us'),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => AboutDSC()));
-                    //Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  title: const Text('Our Team'),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => Team()));
-                  },
-                ),
-              ],
+              child: Image.asset(Assets.vitb_dsc_logo),
             ),
-          ),
-
+            ListTile(
+              title: const Text('About Us'),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AboutDSC()));
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Our Team'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Team()));
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         toolbarHeight: 80,
         elevation: 0,
-        title:
-      Container(
+        title: Container(
           height: 80,
           child: Image.asset(Assets.vitb_dsc_logo),
         ),
         centerTitle: true,
       ),
-      body: screens[index],
-      bottomNavigationBar: NavigationBarTheme(
-        data:
-      NavigationBarThemeData(
-        indicatorColor: Colors.blue.shade200,
-        // iconTheme: MaterialStateProperty(),
-        height: 60,
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+      body: IndexedStack(
+        children: screens,
+        index: current_index,
       ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: Colors.blue.shade200,
+          // iconTheme: MaterialStateProperty(),
+          height: 60,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        ),
         child: NavigationBar(
-          selectedIndex: index,
-          onDestinationSelected: (index)=>setState(()=>this.index=index),
-
+          selectedIndex: current_index,
+          onDestinationSelected: (index) =>
+              setState(() => this.current_index = index),
           destinations: [
-            NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Home', selectedIcon: Icon(Icons.home),),
-            NavigationDestination(icon: Icon(Icons.article_outlined), label: 'Articles',selectedIcon: Icon(Icons.article)),
-            NavigationDestination(icon: Icon(Icons.question_answer_outlined), label: 'Request',selectedIcon: Icon(Icons.question_answer),),
+            NavigationDestination(
+              icon: Icon(Icons.event_outlined),
+              label: 'Events',
+              selectedIcon: Icon(Icons.event),
+            ),
+            NavigationDestination(
+                icon: Icon(Icons.article_outlined),
+                label: 'Articles',
+                selectedIcon: Icon(Icons.article)),
+            NavigationDestination(
+              icon: Icon(Icons.contact_support_outlined),
+              label: 'Request',
+              selectedIcon: Icon(Icons.contact_support),
+            ),
           ],
         ),
       ),
