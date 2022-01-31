16209ccd61bbfc08567f5eb258a877b8914f05cc
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-31 18:56:35 +0530

    UI/UX: Initial design for SignIn Page

--- lib/authentication/GoogleSignInPage.dart
+++ lib/authentication/GoogleSignInPage.dart
@@ -1,3 +1,6 @@
+import 'package:dsc_client/authentication/EmailSignIn.dart';
+import 'package:dsc_client/authentication/EmailSignInPage.dart';
+import 'package:dsc_client/configs/assets.dart';
 import 'package:flutter/material.dart';
 import '../authentication/GoogleSignIn.dart';
 import 'package:provider/provider.dart';
@@ -15,77 +18,81 @@ class _GSignInPageState extends State<GSignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              onPressed: () {
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                provider.googleLogin();
-              },
-
-              child: Container(
-                alignment: Alignment.center,
-                height: 36,
-                // Constructor Call #2
-                child: Text(
-                  'Sign In with Google',
-                  style: TextStyle(
-                      fontSize: 16,
-                      // Constructor Call #3
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(children: [
+                  Container(
+                      height: 130,
+                      width: 200,
+                      child: Image.asset(Assets.vitb_dsc_logo)),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hi \nWelcome Back!',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800, fontSize: 40),
+                      )
+                    ],
+                  )
+                ]),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        final provider = Provider.of<GoogleSignInProvider>(
+                            context,
+                            listen: false);
+                        provider.googleLogin();
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 36,
+                        // Constructor Call #2
+                        child: Text(
+                          'Sign In with Google',
+                          style: TextStyle(
+                              fontSize: 16,
+                              // Constructor Call #3
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                          splashFactory: NoSplash.splashFactory,
+                          // Constructor Call #4
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.amber),
+                          shape:
+                              MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?"),
+                        TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Create an account',
+                              style: TextStyle(color: Colors.redAccent),
+                            ))
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-
-              ////////// Styling //////////
-              style: ButtonStyle(
-                  splashFactory: NoSplash.splashFactory,
-                  // Constructor Call #4
-                  backgroundColor: MaterialStateProperty.all(Color(0xff363250)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: () {
-                print('pressed button -> create an account');
-              },
-
-              child: Container(
-                alignment: Alignment.center,
-                height: 36,
-                // Constructor Call #2
-                child: Text(
-                  'Create an account',
-                  style: TextStyle(
-                      fontSize: 16,
-                      // Constructor Call #3
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
-                ),
-              ),
-
-              ////////// Styling //////////
-              style: ButtonStyle(
-                  splashFactory: NoSplash.splashFactory,
-                  // Constructor Call #4
-                  backgroundColor: MaterialStateProperty.all(Color(0xffb23fff)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ))),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-          ],
-        ),
-      ),
+              ])),
     );
   }
 }
