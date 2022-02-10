e3971dbe74571bf837a49bd0f85aa8d735249c6e
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-02-10 12:01:40 +0530

    misc: Overflow fix

--- lib/authentication/UniversalSignInPage.dart
+++ lib/authentication/UniversalSignInPage.dart
@@ -34,132 +34,137 @@ class _GSignInPageState extends State<GSignInPage> {
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Column(children: [
-                          Container(
-                              height: 130,
-                              width: 200,
-                              child: Image.asset(Assets.vitb_dsc_logo)),
-                          SizedBox(
-                            height: 70,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Hi there,\nWelcome Back!',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w800, fontSize: 40),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 50,
-                          ),
-                          TextField(
-                            controller: emailController,
-                            textInputAction: TextInputAction.next,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: "Email"),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          TextField(
-                            controller: passwordController,
-                            textInputAction: TextInputAction.done,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: "Password"),
-                          ),
-                        ]),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextButton(
-                              onPressed: () {
-                                EmailSignIn();
-                              },
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: 36,
-                                // Constructor Call #2
-                                child: Text(
-                                  'Sign In with Email',
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      // Constructor Call #3
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                              style: ButtonStyle(
-                                  splashFactory: NoSplash.splashFactory,
-                                  // Constructor Call #4
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.amber),
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ))),
-                            ),
+                        SingleChildScrollView(
+                          child: Column(children: [
+                            Container(
+                                height: 130,
+                                width: 200,
+                                child: Image.asset(Assets.vitb_dsc_logo)),
                             SizedBox(
-                              height: 20,
+                              height: 70,
                             ),
-                            TextButton(
-                              onPressed: () {
-                                final provider =
-                                    Provider.of<GoogleSignInProvider>(context,
-                                        listen: false);
-                                provider.googleLogin();
-                              },
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: 36,
-                                // Constructor Call #2
-                                child: Text(
-                                  'Sign In with Google',
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      // Constructor Call #3
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w700),
-                                ),
-                              ),
-                              style: ButtonStyle(
-                                  splashFactory: NoSplash.splashFactory,
-                                  // Constructor Call #4
-                                  backgroundColor:
-                                      MaterialStateProperty.all(Colors.red),
-                                  shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ))),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text("Don't have an account?"),
-                                TextButton(
-                                    onPressed: () {
-                                      Navigator.pushReplacement(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  ESignUpPage()));
-                                    },
-                                    child: Text(
-                                      'Create an account',
-                                      style: TextStyle(color: Colors.green),
-                                    ))
+                                Text(
+                                  'Hi there,\nWelcome Back!',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 40),
+                                )
                               ],
                             ),
-                          ],
+                            SizedBox(
+                              height: 50,
+                            ),
+                            TextField(
+                              controller: emailController,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: "Email"),
+                            ),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            TextField(
+                              controller: passwordController,
+                              textInputAction: TextInputAction.done,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: "Password"),
+                            ),
+                          ]),
+                        ),
+                        SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  EmailSignIn();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  height: 36,
+                                  // Constructor Call #2
+                                  child: Text(
+                                    'Sign In with Email',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        // Constructor Call #3
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                    splashFactory: NoSplash.splashFactory,
+                                    // Constructor Call #4
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.amber),
+                                    shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ))),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  final provider =
+                                      Provider.of<GoogleSignInProvider>(context,
+                                          listen: false);
+                                  provider.googleLogin();
+                                },
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  height: 36,
+                                  // Constructor Call #2
+                                  child: Text(
+                                    'Sign In with Google',
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        // Constructor Call #3
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                    splashFactory: NoSplash.splashFactory,
+                                    // Constructor Call #4
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.red),
+                                    shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ))),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text("Don't have an account?"),
+                                  TextButton(
+                                      onPressed: () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ESignUpPage()));
+                                      },
+                                      child: Text(
+                                        'Create an account',
+                                        style: TextStyle(color: Colors.green),
+                                      ))
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ])),
             );
