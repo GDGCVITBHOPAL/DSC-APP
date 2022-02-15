fa3b868ec4d44921fefcfd640fd39bdff3c12c30
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-02-15 16:09:09 +0530

    Navigation: Simplify in-app navigation and introduce splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dsc_client/authentication/GoogleSignIn.dart';
 import 'package:dsc_client/authentication/SignInCheck.dart';
 import 'package:dsc_client/authentication/Utils.dart';
+import 'package:dsc_client/screens/splash.dart';
 import 'package:dsc_client/utils/sharedPreferences.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -23,53 +24,57 @@ Future<void> main() async {
 class DSC extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-      create: (context) => GoogleSignInProvider(),
-      child: MaterialApp(
-        scaffoldMessengerKey: Utils.messengerKey,
-        debugShowCheckedModeBanner: false,
-        //App Theming
-        theme: ThemeData(
-          brightness: Brightness.light,
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: Colors.blue.shade300,
-          appBarTheme: AppBarTheme(
-            iconTheme: IconThemeData(color: Colors.black),
-            color: Colors.white,
-          ),
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          drawerTheme: DrawerThemeData(backgroundColor: Colors.grey.shade800),
-          scaffoldBackgroundColor: Colors.black,
-          primaryColor: Colors.blue.shade200,
-          cardColor: Colors.grey.shade800,
-          appBarTheme: AppBarTheme(
-            color: Colors.black,
-          ),
-        ),
-        themeMode: ThemeMode.system,
+        create: (context) => GoogleSignInProvider(),
+        child: MaterialApp(
+            scaffoldMessengerKey: Utils.messengerKey,
+            debugShowCheckedModeBanner: false,
+            //App Theming
+            theme: ThemeData(
+              brightness: Brightness.light,
+              scaffoldBackgroundColor: Colors.white,
+              primaryColor: Colors.blue.shade300,
+              appBarTheme: AppBarTheme(
+                iconTheme: IconThemeData(color: Colors.black),
+                color: Colors.white,
+              ),
+            ),
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              drawerTheme:
+                  DrawerThemeData(backgroundColor: Colors.grey.shade800),
+              scaffoldBackgroundColor: Colors.black,
+              primaryColor: Colors.blue.shade200,
+              cardColor: Colors.grey.shade800,
+              appBarTheme: AppBarTheme(
+                color: Colors.black,
+              ),
+            ),
+            themeMode: ThemeMode.system,
 
-        initialRoute: isHomeScreen ? '/' : '/home',
-        onGenerateRoute: (RouteSettings settings) {
-          switch (settings.name) {
-            case '/':
-              return PageRouteBuilder(
-                  pageBuilder: (_, a1, a2) => OnBoardingScreen(),
-                  settings: settings);
-            case '/home':
-              return PageRouteBuilder(
-                  pageBuilder: (_, a1, a2) => SignInCheck(),
-                  settings: settings);
-            case '/eventDetails':
-              return PageRouteBuilder(
-                  pageBuilder: (_, a1, a2) => EventDetails(),
-                  settings: settings);
-            default:
-              return PageRouteBuilder(
-                pageBuilder: (_, a1, a2) => SignInCheck(),
-                settings: settings,
-              );
-          }
-        },
-      ));
+            // initialRoute: isHomeScreen ? '/' : '/home',
+            // onGenerateRoute: (RouteSettings settings) {
+            //   switch (settings.name) {
+            //     case '/':
+            //       return PageRouteBuilder(
+            //           pageBuilder: (_, a1, a2) => OnBoardingScreen(),
+            //           settings: settings);
+            //     case '/home':
+            //       return PageRouteBuilder(
+            //           pageBuilder: (_, a1, a2) => SignInCheck(),
+            //           settings: settings);
+            //     case '/eventDetails':
+            //       return PageRouteBuilder(
+            //           pageBuilder: (_, a1, a2) => EventDetails(),
+            //           settings: settings);
+            //     default:
+            //       return PageRouteBuilder(
+            //         pageBuilder: (_, a1, a2) => SignInCheck(),
+            //         settings: settings,
+            //       );
+            //   }
+            // },
+            title: 'DSC VITB',
+            //Redirect to splash for first-time check
+            home: splash()),
+      );
 }
